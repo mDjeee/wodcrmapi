9e38ad0c69638630f64065e0cccc9362e85d468d
islam <isejtniyazov@mail.ru>
2025-04-08 15:27:36 +0500

    feat: add token to swagger

--- src/main/java/com/example/wodcrmapi/config/SwaggerConfig.java
+++ src/main/java/com/example/wodcrmapi/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.example.wodcrmapi.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,15 +16,24 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("API Title Example")
-                        .description("API Description Example")
-                        .version("API Version")
+                .info(new Info().title("WODCRM API")
+                        .description("API For Managing CRM")
+                        .version("1.0")
                         .contact(new Contact()
-                                .name("API Contact Name")
+                                .name("Islam Seytniyazov")
                                 .url("https://api.contact.url")
                                 .email("medejeee@gmail.com")))
                 .externalDocs(new ExternalDocumentation()
                         .description("API External Documentation")
-                        .url("https://api.external.documentation.url"));
+                        .url("https://api.external.documentation.url"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                );
     }
 }
