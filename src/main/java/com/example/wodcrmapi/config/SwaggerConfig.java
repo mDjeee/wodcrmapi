e321bf3604d712d22ae68f6e32409376ae2ac2f2
islam <isejtniyazov@mail.ru>
2025-04-08 15:22:19 +0500

    feat: add swagger

--- src/main/java/com/example/wodcrmapi/config/SwaggerConfig.java
+++ src/main/java/com/example/wodcrmapi/config/SwaggerConfig.java
@@ -1,12 +1,9 @@
 package com.example.wodcrmapi.config;
 
-import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,31 +11,17 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    GroupedOpenApi publicApi() {
-        return GroupedOpenApi
-                .builder()
-                .group("wodcrm-api")
-                .pathsToMatch("/**")
-                .build();
-    }
-
-    @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("WODCRM API")
-                        .version("1.0")
-                        .description("API for managing browser extensions")
+                .info(new Info().title("API Title Example")
+                        .description("API Description Example")
+                        .version("API Version")
                         .contact(new Contact()
-                                .name("Wod CRM")
-                                .email("wodcrm@gmail.com"))
-                        );
-//                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-//                .components(new Components()
-//                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
-//                                .type(SecurityScheme.Type.HTTP)
-//                                .scheme("bearer")
-//                                .bearerFormat("JWT")
-//                        ));
+                                .name("API Contact Name")
+                                .url("https://api.contact.url")
+                                .email("medejeee@gmail.com")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("API External Documentation")
+                        .url("https://api.external.documentation.url"));
     }
 }
